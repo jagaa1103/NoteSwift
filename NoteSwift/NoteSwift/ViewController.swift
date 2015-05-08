da4adc3218a08e76a59e5906c914ce5809bf4285
Jagaa <jagaa1103@gmail.com>
2015-05-08 17:32:00 +0900

    Start app
    It’s only start

---
+++ NoteSwift/NoteSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteSwift
+//
+//  Created by Enkhjargal Gansukh on 5/8/15.
+//  Copyright (c) 2015 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
